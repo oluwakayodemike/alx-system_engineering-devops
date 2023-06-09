96214abef70e1cd37c1a77b23e1071d7bafb63e7
Oluwakayode Michael <michaelaykay@gmail.com>
2023-06-09 15:50:03 -0700

    0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module provides a function to query the Reddit API and retrieve the number of subscribers for a given subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom User Agent"}  # Set a custom User-Agent header
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
+        return 0
+
