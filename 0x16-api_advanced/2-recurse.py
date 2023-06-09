d8c9a3902a00896525804914956725bcb665c740
Oluwakayode Michael <michaelaykay@gmail.com>
2023-06-09 16:21:23 -0700

    2. Recurse it! - fix recursion checks and pep8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-This module provides a recursive function to query the Reddit API and
+This module provides a recursive function to query the Reddit API
 return a list containing the titles of all hot articles for a given subreddit.
 """
 
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """
-    Recursively queries the Reddit API and returns a list containing
-    the titles of all hot articles for a given subreddit.
+    Recursively queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit.
-        hot_list (list): The list to store the titles of hot articles 
-        (default=[]).
+        hot_list (list): The list to store titles of hot articles (default=[]).
+        after (str): The 'after' parameter to paginate through d API responses
+        (default=None).
 
     Returns:
         list: The list containing the titles of all hot articles.
@@ -24,8 +25,9 @@ def recurse(subreddit, hot_list=[]):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     # Set a custom User-Agent header
     headers = {"User-Agent": "Custom User Agent"}
+    params = {"limit": 100, "after": after}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, params=params)
 
     if response.status_code == 200:
         data = response.json()
@@ -36,9 +38,8 @@ def recurse(subreddit, hot_list=[]):
 
         after = data["data"]["after"]
         if after is not None:
-            return recurse(subreddit, hot_list=hot_list)
+            return recurse(subreddit, hot_list=hot_list, after=after)
         else:
             return hot_list
     else:
         return None
-
