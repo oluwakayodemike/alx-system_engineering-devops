a7675e8168e15b33dfc6f7a5e8fdb5724ac7cdcd
Oluwakayode Michael <michaelaykay@gmail.com>
2023-06-09 16:51:30 -0700

    3. Count it! -- fixing Recursion Check, Output checks & more PEP8 validation

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 This module provides a recursive function to query the Reddit API,
 parse the titles of all hot articles,
@@ -8,7 +9,7 @@ and print a sorted count of given keywords.
 import requests
 
 
-def count_words(subreddit, word_list, after=None, count_dict={}):
+def count_words(subreddit, word_list, after=None, count_dict=None):
     """
     Recursively queries the Reddit API, parses the titles of all hot articles,
     and prints a sorted count of given keywords.
@@ -16,18 +17,17 @@ def count_words(subreddit, word_list, after=None, count_dict={}):
     Args:
         subreddit (str): The name of the subreddit.
         word_list (list): The list of keywords to count.
-
-        after (str): The 'after' parameter to paginate through
-        the API responses (default=None).
-
-        count_dict (dict): The dictionary to store the
-        counts of keywords (default={})/
+        after (str): The 'after' parameter to paginate through the API responses (default=None).
+        count_dict (dict): The dictionary to store the counts of keywords (default=None).
 
     Returns:
         None
     """
+
+    if count_dict is None:
+        count_dict = {}
+
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    # Set a custom User-Agent header
     headers = {"User-Agent": "Custom User Agent"}
     params = {"limit": 100, "after": after}
 
@@ -36,6 +36,7 @@ def count_words(subreddit, word_list, after=None, count_dict={}):
     if response.status_code == 200:
         data = response.json()
         posts = data["data"]["children"]
+
         for post in posts:
             title = post["data"]["title"].lower()
             for word in word_list:
@@ -48,17 +49,11 @@ def count_words(subreddit, word_list, after=None, count_dict={}):
 
         after = data["data"]["after"]
         if after is not None:
-            return count_words{
-                    subreddit,
-                    word_list,
-                    after = after,
-                    count_dict = count_dict
-            }
+            return count_words(subreddit, word_list, after, count_dict)
         else:
-            # Sort by count (descending) and then keyword (ascending)
-            sorted_counts = sorted(
-                count_dict.items(), key=lambda x: (-x[1], x[0]))
+            sorted_counts = sorted(count_dict.items(), key=lambda x: (-x[1], x[0].lower()))
             for keyword, count in sorted_counts:
-                print(f"{keyword}: {count}")
+                print(f"{keyword.lower()}: {count}")
     else:
         return None
+
