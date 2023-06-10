6f3da273544754c23167ee7a2891c74137fe7e65
Oluwakayode Michael <michaelaykay@gmail.com>
2023-06-09 17:17:53 -0700

    3. Count it! -- i thhink this is my last fix :(

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,59 +1,59 @@
 #!/usr/bin/python3
-
 """
-This module provides a recursive function to query the Reddit API,
-parse the titles of all hot articles,
-and print a sorted count of given keywords.
+This module provides a function to count words in all hot posts
+of a given Reddit subreddit.
 """
 
 import requests
 
 
-def count_words(subreddit, word_list, after=None, count_dict=None):
-    """
-    Recursively queries the Reddit API, parses the titles of all hot articles,
-    and prints a sorted count of given keywords.
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Prints counts of given words found in hot posts of a given subreddit.
 
     Args:
-        subreddit (str): The name of the subreddit.
-        word_list (list): The list of keywords to count.
-        after (str): The 'after' parameter to paginate through the API responses (default=None).
-        count_dict (dict): The dictionary to store the counts of keywords (default=None).
-
-    Returns:
-        None
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched thus far.
     """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
 
-    if count_dict is None:
-        count_dict = {}
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Custom User Agent"}
-    params = {"limit": 100, "after": after}
-
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-
-        for post in posts:
-            title = post["data"]["title"].lower()
-            for word in word_list:
-                count = title.count(word.lower())
-                if count > 0:
-                    if word in count_dict:
-                        count_dict[word] += count
-                    else:
-                        count_dict[word] = count
-
-        after = data["data"]["after"]
-        if after is not None:
-            return count_words(subreddit, word_list, after, count_dict)
-        else:
-            sorted_counts = sorted(count_dict.items(), key=lambda x: (-x[1], x[0].lower()))
-            for keyword, count in sorted_counts:
-                print(f"{keyword.lower()}: {count}")
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
     else:
-        return None
-
+        count_words(subreddit, word_list, instances, after, count)
